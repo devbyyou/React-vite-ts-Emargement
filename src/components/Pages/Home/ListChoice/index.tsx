bb6f60d1b175c3f64061dc863b8d581cfaca6456
devbyyou <lyy.pro@gmail.com>
2023-11-09 21:24:09 +0100

    modification css de la partie profil

--- src/components/Pages/Home/ListChoice/index.tsx
+++ src/components/Pages/Home/ListChoice/index.tsx
@@ -3,6 +3,7 @@ import { FcPlanner } from 'react-icons/fc';
 import './index.scss';
 import { BiSolidChevronRight } from 'react-icons/bi';
 import { FaRegEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function ListChoice() {
   return (
@@ -10,23 +11,23 @@ function ListChoice() {
       <h2>Accès rapide</h2>
       <div className="listChoice__card">
         <FcPlanner className="plannerLogo" />
-        <div className="listChoice__card-content">
+        <Link to="/presents" className="listChoice__card-content">
           <p>Gérer vos séances</p>
           <div className="listChoice__card-items">
             <span>Tableau de bord</span>
           </div>
-        </div>
+        </Link>
         <BiSolidChevronRight className="chevronLogo" />
       </div>
 
       <div className="listChoice__card">
         <FaRegEdit className="editLogo" />
-        <div className="listChoice__card-content">
+        <Link to="/equipes" className="listChoice__card-content">
           <p>Modifier vos joueurs</p>
           <div className="listChoice__card-items">
             <span>Centre joueurs</span>
           </div>
-        </div>
+        </Link>
         <BiSolidChevronRight className="chevronLogo" />
       </div>
     </div>
