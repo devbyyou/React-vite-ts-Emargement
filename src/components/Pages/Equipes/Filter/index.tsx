bc7e1748175ddbc00f07bb62da583207203d2eec
devbyyou <lyy.pro@gmail.com>
2023-12-13 13:28:30 +0100

    ajout de key unique pour le filtre

--- src/components/Pages/Equipes/Filter/index.tsx
+++ src/components/Pages/Equipes/Filter/index.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import './index.scss';
+import { useAppSelector } from '../../../../hooks/redux';
 
 function Filter() {
+  const user = useAppSelector((state) => state.user.token.user);
+  const { equipes } = user;
+  // const { categories } = equipes;
+  const categories = equipes.map((listeEquipes) => listeEquipes.categories);
+  // console.log(categories);
+  // astuce pour ne pas répéter plusieurs fois la même catégories
+  // Utilisons un ensemble pour suivre les clés déjà rencontrées
+  const seenKeys = new Set();
+  // Filtrons les catégories pour ne montrer que celles avec des clés uniques
+  const uniqueCategories = categories.filter((category) => {
+    const isUnique = !seenKeys.has(category.id);
+    seenKeys.add(category.id);
+    return isUnique;
+  });
+
   return (
     <div className="filter">
       <h2>Filtrer par</h2>
+
       <div className="card__filer">
         <p>CATEGORIES</p>
         <form className="card__filer-content">
-          <div className="card__filer-content-element">
-            <label htmlFor="cdm">CDM</label>
-            <input type="radio" id="cdm" name="cdm" value="cdm" />
-          </div>
-          <div className="card__filer-content-element">
-            <label htmlFor="senior">Sénior</label>
-            <input type="radio" id="senior" name="senior" value="senior" />
-          </div>
-          <div className="card__filer-content-element">
-            <label htmlFor="u20">U20</label>
-            <input type="radio" id="u20" name="u20" value="u20" />
-          </div>
+          {
+                 uniqueCategories.map((element) => (
+                   <div key={element.id} className="card__filer-content-element">
+                     <label htmlFor="cdm">{element.nom}</label>
+                     <input type="checkbox" id="cdm" name="category" value="cdm" />
+                   </div>
+                 ))
+          }
         </form>
       </div>
 
@@ -27,20 +40,21 @@ function Filter() {
         <p>TOTAL EFFECTIF</p>
         <form className="card__filer-content">
           <div className="card__filer-content-element">
-            <label htmlFor="cdm">0 - 20</label>
-            <input type="radio" id="cdm" name="cdm" value="cdm" />
+            <label htmlFor="0-20">0 - 10</label>
+            <input type="checkbox" id="0-20" name="totalEffectif" value="0-20" />
           </div>
           <div className="card__filer-content-element">
-            <label htmlFor="senior">20 - 40</label>
-            <input type="radio" id="senior" name="senior" value="senior" />
+            <label htmlFor="20-40">10 - 40</label>
+            <input type="checkbox" id="20-40" name="totalEffectif" value="20-40" />
           </div>
           <div className="card__filer-content-element">
-            <label htmlFor="u20">+40</label>
-            <input type="radio" id="u20" name="u20" value="u20" />
+            <label htmlFor="40plus">+40</label>
+            <input type="checkbox" id="40plus" name="totalEffectif" value="40plus" />
           </div>
         </form>
       </div>
     </div>
+
   );
 }
 
