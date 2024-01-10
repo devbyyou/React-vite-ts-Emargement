e74ae3d7b5da0673cafdbc26995afe5617202ded
devbyyou <lyy.pro@gmail.com>
2024-01-10 14:33:49 +0100

    ajout possibilité filtrage par equipe

--- src/components/Pages/Presents/LIstPresents/index.tsx
+++ src/components/Pages/Presents/LIstPresents/index.tsx
@@ -1,19 +1,16 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './index.scss';
 import cn from 'classnames';
 import { useAppSelector } from '../../../../hooks/redux';
 import functionConverteDate from '../../Home/MembersList/ConverteDate';
 
 function ListPresents() {
+  const [valueOption, setvalueOption] = useState('');
   const equipes = useAppSelector((state) => state.equipes.equipes);
   const findAllSeances = equipes.map((seances) => seances.seances);
   const findAllJoueurs = equipes.map((seances) => seances.joueurs);
-
-  const findAllPresences = findAllSeances.map((seances) => seances.map((findpresences) => {
-    const presence = findpresences.presences;
-    return presence.filter((present) => present);
-  }));
   const findSeances = (seanceId: number) => {
     const filteredHeures = findAllSeances
       .flatMap((seance) => seance
@@ -36,20 +33,31 @@ function ListPresents() {
       .map((joueur) => joueur.tel);
     return filteredPlayer.length > 0 ? filteredPlayer.join(', ') : 'No matching seances found';
   };
-
+  const filteredByEquipe = findAllSeances
+    .flatMap((seances) => seances
+      .filter((seance) => seance.equipe_id.toString() === valueOption.toString()));
+  const handleChangeOption = (event: ChangeEvent<HTMLSelectElement>) => {
+    const valueSelect = event.target.value;
+    setvalueOption(valueSelect);
+  };
   return (
     <div className="listPresents">
 
-      <div className="listPresents__timer">Début du prochaine entrainement dans : 3:05 min</div>
+      <div className="listPresents__timer">
+        Début du prochaine entrainement dans : 3:05 min
+      </div>
       <div className="listPresents__table">
-
-        <select className="teamSelect" name="teamSelect" id="teamSelect">
+        <select onChange={handleChangeOption} value={valueOption} className="teamSelect" name="teamSelect" id="teamSelect">
           <option value="">Choisi ton équipe</option>
-          <option value="cdm">CDM</option>
-          <option value="senior">Sénior</option>
-          <option value="u20">U20</option>
+          { equipes.map((equipe) => (
+            <option
+              value={equipe.id}
+              key={equipe.id}
+            >
+              {equipe.nom}
+            </option>
+          ))}
         </select>
-
         <div className="listPresents__row listPresents__label">
           <div className="listPresents__cell">Nom</div>
           <div className="listPresents__cell">Séance Prévu à</div>
@@ -58,40 +66,33 @@ function ListPresents() {
           <div className="listPresents__cell">Absent</div>
           <div className="listPresents__cell">Retard</div>
         </div>
+        {filteredByEquipe.map((seances) => seances.presences.map((player) => (
+          <div
+            key={player.id}
+            className={cn('listPresents__row', {
+              present: player.statut === 'PRESENT',
+              retard: player.retard === 'RETARD',
+              absent: player.absence === 'ABSENT',
+            })}
+          >
+            <div className="listPresents__cell">
+              {findPlayer(player.joueur_id)}
+              <div className="listPresents__table__email">{findPlayerTel(player.joueur_id)}</div>
+            </div>
+            <div className="listPresents__cell">
+              { findSeances(player.seance_id)}
+            </div>
+            <div className="listPresents__cell">
+              {functionConverteDate.heureHiver(player.updated_at, 1, 'HH:mm')}
+            </div>
+            <div className="listPresents__cell">
+              {player.statut}
+            </div>
+            <div className="listPresents__cell">{player.absence}</div>
+            <div className="listPresents__cell">{player.retard}</div>
 
-        {findAllPresences.map((sessions, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <React.Fragment key={index}>
-            {sessions.map((players) => players.map((player) => (
-              <div
-                key={player.id}
-                className={cn('listPresents__row', {
-                  present: player.statut === 'PRESENT',
-                  retard: player.retard === 'RETARD',
-                  absent: player.absence === 'ABSENT',
-                })}
-              >
-                <div className="listPresents__cell">
-                  {findPlayer(player.joueur_id)}
-                  <div className="listPresents__table__email">{findPlayerTel(player.joueur_id)}</div>
-                </div>
-                <div className="listPresents__cell">
-                  { findSeances(player.seance_id)}
-                </div>
-                <div className="listPresents__cell">
-                  {functionConverteDate.heureHiver(player.updated_at, 1, 'HH:mm:ss')}
-                </div>
-                <div className="listPresents__cell">
-                  {player.statut}
-                </div>
-                <div className="listPresents__cell">{player.absence}</div>
-                <div className="listPresents__cell">{player.retard}</div>
-
-              </div>
-            )))}
-          </React.Fragment>
-        ))}
-
+          </div>
+        )))}
       </div>
     </div>
   );
